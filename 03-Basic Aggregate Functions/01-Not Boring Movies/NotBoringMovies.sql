49fefa7a998ad401cf92b77b85f30195eadca282
Amit Hegde <amithegde777@gmail.com>
2023-08-08 00:09:27 +0530

    not boring movies

---
+++ 03-Basic Aggregate Functions/01-Not Boring Movies/NotBoringMovies.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT id,movie,description,rating FROM Cinema WHERE id%2!=0 AND description!="boring" ORDER BY rating DESC;
+
